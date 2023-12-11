4eca98a30dbbe95e6c76b86c1be0d27e87657c12
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 03:17:50 +0100

    mock empls

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { IEmployee } from '../../shared/interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-employees',
-  standalone: true,
-  imports: [],
-  templateUrl: './employees.component.html',
-  styleUrl: './employees.component.scss'
+	selector: 'app-employees',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './employees.component.html',
+	styleUrl: './employees.component.scss',
 })
 export class EmployeesComponent {
+	employees: IEmployee[] = [];
 
+	constructor() {
+		this.employees = [
+			{
+				firstName: 'Frank',
+				lastName: 'Holander',
+				notes: "These are Frank's notes.",
+			},
+			{
+				firstName: 'Selma',
+				lastName: 'Schmidt',
+				notes: "These are Selma's notes.",
+			},
+		];
+	}
 }
