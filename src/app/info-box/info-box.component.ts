a0171d114e61c0e0fb14be7f26fea2df6f0d1b33
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 01:20:46 +0100

    ngswitch

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -16,7 +16,7 @@ export class InfoBoxComponent {
 	color = '';
 
 	ngOnInit() {
-		this.color = this.status === 'danger' ? 'red' : 'lightgreen';
+		this.color = this.status === 'danger' ? 'tomato' : this.status === 'success' ? 'lightgreen' : '#222';
 	}
-}
+}	
 
