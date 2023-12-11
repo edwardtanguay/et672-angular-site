0d45a798a49ca3187b82037e9db124f3c47427dc
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 02:50:54 +0100

    germandate

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [ CommonModule],
+	imports: [CommonModule],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.scss',
 })
