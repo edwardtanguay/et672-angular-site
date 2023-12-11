4cc90d2f27ac401e6ea3e862edbc59272e80a72c
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 02:11:10 +0100

    styled menu

---
+++ src/app/top-menu/top-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-top-menu',
+  standalone: true,
+  imports: [ RouterModule],
+  templateUrl: './top-menu.component.html',
+  styleUrl: './top-menu.component.scss'
+})
+export class TopMenuComponent {
+
+}
