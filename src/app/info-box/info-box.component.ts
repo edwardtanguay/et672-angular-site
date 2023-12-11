3d8bc1920b3cf3c2fd4814742b13e73bb3c37ff6
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 01:03:31 +0100

    ngstyle

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -1,13 +1,22 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-info-box',
 	standalone: true,
-	imports: [],
+	imports: [CommonModule],
 	templateUrl: './info-box.component.html',
 	styleUrl: './info-box.component.scss',
 })
 export class InfoBoxComponent {
+	@Input() status = '';
 	@Input() title = 'Info Box';
 	@Input() body = '(watch this space...)';
+
+	color = '';
+
+	ngOnInit() {
+		this.color = this.status === 'danger' ? 'darkred' : 'lightgreen';
+	}
 }
+
