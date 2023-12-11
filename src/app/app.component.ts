da8896a599cc3cdc7b4a13e29aa03b0ddb7202ca
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-11 01:56:15 +0100

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { InfoBoxComponent } from './info-box/info-box.component';
+import { TopMenuComponent } from "./top-menu/top-menu.component";
 
 @Component({
-	selector: 'app-root',
-	standalone: true,
-	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss',
-	imports: [CommonModule, RouterOutlet, InfoBoxComponent],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, InfoBoxComponent, TopMenuComponent]
 })
 export class AppComponent {
 	title = 'The Angular Site';
